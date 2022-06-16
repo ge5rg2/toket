d8a9c92a5e6f422ff1c562adac665deea6e1ddef
ge5rg2 <kghee9612@gmail.com>
2022-06-16 14:02:48 +0900

    add auto carousel

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "./Footer";
+import dummyImages from "./static/dummyImage";
+import styled from "styled-components";
+import style from "./Home.module.scss"
 
 const Home = () => {
   return (
@@ -19,6 +22,22 @@ const Home = () => {
       </div>
       <div id="imageDiv">
         <h1>Image</h1>
+        <div className={`${style.body}`}>
+          <div className={`${style.slider}`}>
+            <div className={`${style.slide_track}`}>
+          {dummyImages.map((el) => {
+            return (
+                  <div className={`${style.slide}`}>
+                    <img
+                      src={el.picture}
+                      style={{ width: "100%", height: "100%", alt: "" }}
+                    ></img>
+                  </div>
+            );
+          })}
+          </div>
+          </div>
+          </div>
       </div>
       <Footer />
     </div>
---
+++ src/routes/static/dummyImage.js
@@ -0,0 +1,44 @@
+const dummyImages = [
+  {
+    id: 1,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_1.jpg",
+  },
+  {
+    id: 2,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_2.jpg",
+  },
+  {
+    id: 3,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_3.jpg",
+  },
+  {
+    id: 4,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_5.jpg",
+  },
+  {
+    id: 5,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_6.jpg",
+  },
+  {
+    id: 6,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_7.jpg",
+  },
+  {
+    id: 7,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_8.jpg",
+  },
+  {
+    id: 8,
+    picture:
+      "http://te-ket.com/web/imagelookbook/Plan05/%5Bteket%5D%20Plan05_image_11.jpg",
+  },
+];
+
+export default dummyImages;
